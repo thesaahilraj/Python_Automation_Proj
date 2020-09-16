dc85736d182c3b47fd457d48610ca220c3c9ca19
= <=>
2020-09-16 15:40:16 +0530

    Final Commit

--- Gmail_Automation.py
+++ Gmail_Automation.py
@@ -1,4 +1,4 @@
-#Gmail Automation
+# Gmail Automation
 #By : github.com/thesaahilraj
 
 '''
@@ -6,17 +6,18 @@ A simple Automation code used to Send Email using Gmail smtp
 '''
 
 
-#import smtp Library
+# import smtp Library
 import smtplib
 
-#create an Object
+# create an Object
 conn = smtplib.SMTP("smtp.gmail.com")
 conn.ehlo()
 conn.starttls()
 
-#Login with the Senders Email Address
-conn.login("sendersemail@gmail.com","password123")
+# Login with the Senders Email Address
+conn.login("sendersemail@gmail.com", "password123")
 
-#Send Mail
-# obj.send("sender's mail", "Receiver's Mail" , "Message")
-conn.sendmail("sendersemail@gmail.com", "thesaahilraj@gmail.com" ,"Subject: Automation \n\n Body: This mail is Sent using gmail Automation")
+# Send Mail
+# obj.sendmail("sender's mail", "Receiver's Mail" , "Message")
+conn.sendmail("sendersemail@gmail.com", "thesaahilraj@gmail.com",
+              "Subject: Automation \n\n Body: This mail is Sent using gmail Automation")
